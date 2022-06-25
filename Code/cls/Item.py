73f8bb60c8bfc89b3aecf10874a615c0586f3255
henrydatei <31316025+henrydatei@users.noreply.github.com>
2022-06-25 17:13:12 +0200

    removed old code with other solution representation

--- Code/cls/Item.py
+++
@@ -1,7 +0,0 @@
-from dataclasses import dataclass
-
-@dataclass
-class Item:
-    id: int
-    weight: int
-    profit: int
--- Code/cls/Knapsack.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass
-import dataclasses
-
-@dataclass
-class Knapsack:
-    id: int
-    capacity: int
-    penalty: int
